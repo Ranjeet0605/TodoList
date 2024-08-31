0137bf006a4ae8b6bee30b1e761ba5621d75b384
Ranjeet0605 <111526289+Ranjeet0605@users.noreply.github.com>
2024-09-01 03:07:59 +0530

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connection = async(req,res)=>{
     try{
 
-   await mongoose.connect("mongodb+srv://r6306344:rjHK4gmj9gcvZORy@cluster0.o50ap.mongodb.net/ToDoList").then(()=>{
+   await mongoose.connect("mongodb+srv://username:password@cluster0.o50ap.mongodb.net/ToDoList").then(()=>{
         console.log("Database is connected");
     })
 }
